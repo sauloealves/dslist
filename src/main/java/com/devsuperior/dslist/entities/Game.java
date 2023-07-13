aa618e76b2470b000604a4d751c5333955c56a4b
saulo <sauloealves@gmail.com>
2023-07-12 21:09:16 -0300

    Seed games

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -8,11 +8,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tb_game")
+@Table(name = "tb_game")
 public class Game {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     private String title;
@@ -22,25 +23,35 @@ public class Game {
 
     private String genre;
 
+    private String platforms;
+
+    private Double score;
+
     private String imgUrl;
 
+    @Column(columnDefinition="TEXT")
     private String shortDescription;
 
+    @Column(columnDefinition="TEXT")
     private String longDescription;
 
     public Game() {
-    }
+    }  
 
-    public Game(Long id, String title, Integer year, String genre, String imgUrl, String shortDescription,
-            String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+            String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
+
+
 
     public Long getId() {
         return id;
@@ -98,6 +109,22 @@ public class Game {
         this.longDescription = longDescription;
     }
 
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -123,6 +150,4 @@ public class Game {
         return true;
     }
 
-
-    
 }
